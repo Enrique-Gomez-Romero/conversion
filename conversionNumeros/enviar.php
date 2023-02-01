be4e6a797b9acfabd00c3ce41640d83927ab8d83
Henry <89955423+Enrique-Gomez-Romero@users.noreply.github.com>
2023-01-31 18:57:51 -0600

    Add files via upload

--- conversionNumeros/enviar.php
+++ conversionNumeros/enviar.php
@@ -1,49 +1,68 @@
-<?php
-include("conexionBD.php");
-//enviar los datos a la base de datos
-$ingresado = $_POST['entrada1'];
-$r = var_export ($resultado , true);
-//pasa r a una cadena de texto
-// $resultado = trim($r);
-$resultado = $r;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="estilos.css">
+    <title>Document</title>   
+</head>
+<body>
+    <div class=" text-center mb-4"><em class="fs-3">Resultados obtenidos</em></div>
+    <div class="container">
+        <div class="row">
+        <?php
+            include("conexionBD.php");
+            //enviar los datos a la base de datos
+            $ingresado = $_POST['entrada1'];
+            $r = var_export ($resultado , true);
+            //pasa r a una cadena de texto
+            // $resultado = trim($r);
+            $resultado = $r;
 
-date_default_timezone_set('America/Mexico_City');
-//sacar fecha
-$fecha = date("Y-m-d");
-//sacar hora
-$hora = date("H:i:s");
-echo "<br/>" . $resultado;
-echo "<br/>" . $ingresado;
-echo "<br/>" . $fecha;
-echo "<br/>" . $hora;
+            date_default_timezone_set('America/Mexico_City');
+            //sacar fecha
+            $fecha = date("Y-m-d");
+            //sacar hora
+            $hora = date("H:i:s");
+            echo" <div class='col-md-6 col-sm-12 col-lg-6 text-center mt-5'>
+                    <div class='bg-light p-2 rounded fs-4'>
+                        <label> <em> Resultado: $resultado </em> </label>
+                    </div>
+                  </div>
+                  <div class='col-md-6 col-sm-12 col-lg-6 text-center mt-5'>
+                    <div class='bg-light p-2 rounded fs-4'> 
+                        <label> <em> Dato ingresado:  $ingresado </em> </label>
+                    </div>
+                  </div>
+                  <div class='col-md-12 col-sm-12 col-lg-12 text-center mt-5 fs-3'> <em> Datos extra </em> </div>
+                  <div class='col-md-6 col-sm-12 col-lg-6 text-center mt-5'>
+                    <div class='bg-light p-2 rounded fs-4'>
+                        <label> <em> Fecha: $fecha </em> </label>
+                    </div>
+                  </div>
+                  <div class='col-md-6 col-sm-12 col-lg-6 text-center mt-5'>
+                    <div class='bg-light p-2 rounded fs-4'>
+                        <label> <em> Hora: $hora </em> </label>
+                    </div>
+                  </div>";
 
-// insertar datos en la base de datos
-// $sql_prepare = $conexion->prepare("INSERT INTO datos(ingresado, resultado, fecha, hora) VALUES (:ingresado,:resultado, :fecha, :hora)");
-// $sql_prepare->bindParam(':ingresado', $ingresado);
-// $sql_prepare->bindParam(':resultado', $resultado);
-// $sql_prepare->bindParam(':fecha', $fecha);
-// $sql_prepare->bindParam(':hora', $hora);
-// if(
-//     $sql_prepare->execute()){
-//     echo "Datos guardados";
-//     }else{
-//         echo "un error";
-//     }
-$insertar = "INSERT INTO datos (ingresado, resultado, fecha, hora) VALUES ('{$ingresado}', {$resultado}, '{$fecha}', '{$hora}')";
+
+            $insertar = "INSERT INTO datos (ingresado, resultado, fecha, hora) VALUES ('{$ingresado}', {$resultado}, '{$fecha}', '{$hora}')";
 
 
-// verificar si se insertaron los datos
-// $verificar = mysqli_query($conexion, $insertar);
-if (($result = mysqli_query($conexion, $insertar)) === false) {
-    die(mysqli_error($conexion));
-}else{
-    echo "Datos guardados";
-}
-// if($verificar) {
-//     echo "Datos guardados";	
+            if (($result = mysqli_query($conexion, $insertar)) === false) {
+                die(mysqli_error($conexion));
+            }else{
+                // echo " <div class='mt-5 fs-5'> <em> Datos guardados correctamente </em> </div> ";
+            }
+        ?>
+        </div>
+        
+        <div class="text-center"> <a href="index.php"  class="btn btn-primary mt-5"> Calcular nuevo dato </a></div>
+    </div>
+</body>
+</html>
 
-// }else{
-//     echo "Datos no guardados";
-// }
 
-?>
--- conversionNumeros/index.php
+++ conversionNumeros/index.php
@@ -4,28 +4,47 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="estilos.css">
     <title>Document</title>   
 </head>
 <body>
 
 <form action="calcular.php" method="POST" name="Fomulario">
-    Desde
-    <select class="form-select" aria-label="Default select example" name="op1" id="base">
-        <option value="bin">Binario</option>
-        <option value="dec">Decimal</option>
-        <option value="oct">Octal</option>
-        <option value="hexa">Hexadecimal</option>
-    </select>
-    Para
-    <select class="form-select" aria-label="Default select example" name="op2" id="remove">
-        <option value="bin" disabled>Binario</option>
-        <option value="dec">Decimal</option>
-        <option value="oct">Octal</option>
-        <option value="hexa">Hexadecimal</option>
-    </select>
-        <input type="text" class="form-control" placeholder="Ingrese el numero" name="entrada1" required>
-        <input type="submit" class="btn btn-primary" value="Convertir">
+    <div class="container mt-5">
+        <div class="text-center mb-5">
+            <em class="fs-3">Calculadora de conversion de numeros</em>
+        </div>
+        <div class="row">
+            <div class="col-md-4 col-sm-12 col-lg-4">
+                <em class="text-center fs-5"> Desde: </em>
+                    <select class="form-select mt-4" aria-label="Default select example" name="op1" id="base">
+                        <option value="bin">Binario</option>
+                        <option value="dec">Decimal</option>
+                        <option value="oct">Octal</option>
+                        <option value="hexa">Hexadecimal</option>
+                    </select>
+            </div>
+            <div class="col-md-4 col-sm-12 col-lg-4">
+                <em class="text-center fs-5"> Para: </em>
+                    <select class="form-select mt-4" aria-label="Default select example" name="op2" id="remove">
+                        <option value="bin" disabled>Binario</option>
+                        <option value="dec">Decimal</option>
+                        <option value="oct">Octal</option>
+                        <option value="hexa">Hexadecimal</option>
+                    </select>
+            </div>
+            <div class="col-md-4 col-sm-12 col-lg-4">
+                <input type="text" class="form-control mt-5" placeholder="Ingrese el numero" name="entrada1" required>
+            </div>
+        </div>
+
+        <div class=" text-center mt-5"> 
+            <input type="submit" class="btn btn-primary" value="Convertir"> 
+        </div>
+    </div>
 </form>
+
 <script>
     const selectBase = document.getElementById('base');
     const selectRemove = document.getElementById('remove');
